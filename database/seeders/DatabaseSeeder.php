1a7ff9e804691c7a234d2c346e2f585932b87116
Van Quang <quangvan12102001@gmail.com>
2023-03-10 17:29:10 +0700

    Code AdminSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,19 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Schema::disableForeignKeyConstraints();
+        $this->call(AdminSeeder::class);
+        Schema::enableForeignKeyConstraints();
     }
 }
