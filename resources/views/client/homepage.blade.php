7d971ff29e6cdab0ea9e8c48cfd62a160868e20b
Quang <quangvan12102001@gmail.com>
2023-04-06 17:17:56 +0700

    Code ChatBox

--- resources/views/client/homepage.blade.php
+++ resources/views/client/homepage.blade.php
@@ -505,4 +505,17 @@
     @include('client.share.js')
     @yield('js')
 </body>
+ <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/64102cc231ebfa0fe7f27148/1grfitn6h';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*'); 
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </html>
